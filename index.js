1325db9170592e5cd538a0888e3fc93fdf068192
ASMIttehad <ittehad.akash@gmail.com>
2022-12-18 21:07:51 +0100

    Individual news api added

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const courses = require("./courses.json");
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
+app.get("/courses/", (req, res) => {
+  res.send(courses);
+});
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const eachCat = courses.filter((cat) => cat.category_id == id);
+  res.send(eachCat);
+});
+app.get("/eachCourses/:id", (req, res) => {
+  const id = req.params.id;
+  const eachCat = courses.find((cat) => cat._id == id);
   res.send(eachCat);
 });
 
